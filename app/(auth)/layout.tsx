4c8a271d3c80d41c72e09352a1714ecef65910ee
Mert Can <107743711+Underprotection-TR@users.noreply.github.com>
2025-02-24 05:24:04 +0300

    Add Clerk authentication and update Convex user management

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,21 @@
-export default function AuthLayout({
+import Image from "next/image";
+
+export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div>
-      <main>{children}</main>
-    </div>
+    <main className="relative h-screen w-full">
+      <div className="absolute size-full">
+        <Image
+          src="/images/bg-img.png"
+          alt="background"
+          fill
+          className="size-full"
+        />
+      </div>
+      {children}
+    </main>
   );
 }
